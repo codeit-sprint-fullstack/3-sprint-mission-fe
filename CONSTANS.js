41eea3b0b5b1085e12030b8bab297b4b8cb1c348
KJY <139238536+dani784601@users.noreply.github.com>
2024-10-01 15:19:13 +0900

    feat: 스프린트 미션 3 심화 요구사항 - 오류 메시지 모달 구현

--- CONSTANS.js
+++ CONSTANS.js
@@ -6,3 +6,18 @@ document.title = SITE_TITLE;
 if (document.getElementById('copyright-year')) {
   document.getElementById('copyright-year').innerText = `©codeit - ${year}`;
 }
+
+const USER_DATA = [
+  { email: 'codeit1@codeit.com', password: 'codeit101!' },
+  { email: 'codeit2@codeit.com', password: 'codeit202!' },
+  { email: 'codeit3@codeit.com', password: 'codeit303!' },
+  { email: 'codeit4@codeit.com', password: 'codeit404!' },
+  { email: 'codeit5@codeit.com', password: 'codeit505!' },
+  { email: 'codeit6@codeit.com', password: 'codeit606!' },
+];
+
+const USER_DATA_MAP = new Map();
+
+for (const user of USER_DATA) {
+  USER_DATA_MAP.set(user.email, user.password);
+}
