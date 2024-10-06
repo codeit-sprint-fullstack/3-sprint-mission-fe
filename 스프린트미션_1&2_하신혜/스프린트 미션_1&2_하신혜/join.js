53c31fb20c4278c36f4053ea2e7acfc873759e23
Aventurine91 <mandely01@gmail.com>
2024-10-06 23:34:01 +0900

    basic-하신혜-sprint1

--- 스프린트미션_1&2_하신혜/스프린트 미션_1&2_하신혜/join.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.toggle-password').forEach(function(button) {
-  button.addEventListener('click', function() {
-      const passwordInput = this.previousElementSibling; 
-      const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-      passwordInput.setAttribute('type', type);
-      this.querySelector('img').alt = type === 'password' ? '토글패스워드' : '비밀번호 숨기기';
-  });
-});
