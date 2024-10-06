6c2da54d8c19e859153810ba508033eb9ae4fb29
Daewony <alexnori.com@gmail.com>
2024-10-06 17:00:31 +0900

    feat: 유저 데이터 함수 추가

---
+++ data/users.js
@@ -0,0 +1,10 @@
+export const USER_DATA = [
+  { email: 'codeit1@codeit.com', password: 'codeit101!' },
+  { email: 'codeit2@codeit.com', password: 'codeit202!' },
+  { email: 'codeit3@codeit.com', password: 'codeit303!' },
+  { email: 'codeit4@codeit.com', password: 'codeit404!' },
+  { email: 'codeit5@codeit.com', password: 'codeit505!' },
+  { email: 'codeit6@codeit.com', password: 'codeit606!' },
+];
+
+// 고민: export const 하는 방법과 export default 하는 방법 중 어떤 것이 더 좋을까?
