d6a008cf18db6388dcab48836922522d0e6927e3
heonq <gkagjsrb@naver.com>
2024-10-10 11:49:45 +0900

    refactor: getArticleList,getArticle 함수의 불필요한 async/await 제거

--- assets/scripts/apis/ArticleService.js
+++ assets/scripts/apis/ArticleService.js
@@ -1,7 +1,7 @@
 import URLS from "../constants/url.js";
 
-const getData = async (fetchFunction) => {
-  const data = await fetchFunction()
+const getData = (fetchFunction) => {
+  const data = fetchFunction()
     .then((response) => {
       if (response.status >= 300 || response.status < 200) {
         if (response.status === 404) throw new Error("게시글을 찾을 수 없음");
@@ -13,14 +13,14 @@ const getData = async (fetchFunction) => {
   return data;
 };
 
-export const getArticleList = async (page = 1, pageSize = 100, keyword = "") => {
+export const getArticleList = (page = 1, pageSize = 100, keyword = "") => {
   const URL = `${URLS.articles}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`;
-  const data = getData(async () => await fetch(URL));
+  const data = getData(() => fetch(URL));
   return data;
 };
 
-export const getArticle = async (id = 0) => {
+export const getArticle = (id = 0) => {
   const URL = `${URLS.articles}/${id}`;
-  const data = getData(async () => await fetch(URL));
+  const data = getData(() => fetch(URL));
   return data;
 };
