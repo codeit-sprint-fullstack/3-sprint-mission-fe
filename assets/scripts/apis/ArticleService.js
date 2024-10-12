1298f2d0e2b39666100d122082577a790e5a7d44
heonq <gkagjsrb@naver.com>
2024-10-12 13:23:44 +0900

    refactor: ArticleService api 리팩토링

--- assets/scripts/apis/ArticleService.js
+++ assets/scripts/apis/ArticleService.js
@@ -1,45 +1,32 @@
 import api from "./common.js";
 import URLS from "../constants/url.js";
 
-const getData = async (url) => {
-  return api
-    .get(url)
-    .then((response) => response.data)
-    .catch((e) => console.log(e.message));
+const handleResponse = (apiMethod) => {
+  return apiMethod.then((response) => response.data).catch((e) => console.log(e.message));
 };
 
 export const getArticleList = (page = 1, pageSize = 100, keyword = "") => {
   const URL = `${URLS.articles}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`;
-  const data = getData(URL);
+  const data = handleResponse(api.get(URL));
   return data;
 };
 
 export const getArticle = (id) => {
   const URL = `${URLS.articles}/${id}`;
-  const data = getData(URL);
+  const data = handleResponse(api.get(URL));
   return data;
 };
 
 export const createArticle = (body) => {
-  const data = api
-    .post(URLS.articles, body)
-    .then((response) => response.data)
-    .catch((e) => console.log(e.message));
+  const data = handleResponse(api.post(URLS.articles, body));
   return data;
 };
 
 export const patchArticle = async (id, body) => {
-  const data = api
-    .patch(`${URLS.articles}/${id}`, body)
-    .then((response) => response.data)
-    .catch((e) => console.log(e.message));
+  const data = handleResponse(api.patch(`${URLS.articles}/${id}`, body));
   return data;
 };
 
-export const deleteArticle = async (id) => {
-  const data = await api
-    .delete(`${URLS.articles}/${id}`)
-    .then((response) => response.data)
-    .catch((e) => console.log(e.message));
-  return data;
+export const deleteArticle = (id) => {
+  handleResponse(api.delete(`${URLS.articles}/${id}`));
 };
