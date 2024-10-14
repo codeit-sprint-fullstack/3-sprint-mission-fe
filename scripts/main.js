dffa1373cac6b73ceac204a3bb26c18d4a69e0e2
Yousuk <yousuk88@gmail.com>
2024-10-14 16:47:52 +0900

    fix: getArticle func (GET method), add: createArticle func (POST method)

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,14 @@
-import { getArticleList, getArticle } from "../services/articleService.js";
+import { getArticleList, getArticle, createArticle } from "../services/articleService.js";
 
 
-const temp1 = await getArticleList(1, 3, '마이크')
-console.log(temp1)
+// const temp1 = await getArticleList(1, 3, '마이크')
+// console.log(temp1)
+getArticleList(1, 3, '마이크')
 
-const temp2 = await getArticle(98)
-console.log(temp2)
+
+// const temp2 = await getArticle(126)
+// console.log(temp2)
+getArticle(126);
+
+
+createArticle('긴급속보', '비트코인 3억 돌파', 'bitcoin.jpg');
--- services/articleService.js
+++ services/articleService.js
@@ -1,17 +1,23 @@
 const BASE_URL = 'https://sprint-mission-api.vercel.app/articles'
 
+
+
 export function getArticleList(page, pageSize, keyword) {
   const url = `${BASE_URL}?page=${page}&pageSize=${pageSize}&keyword=${encodeURIComponent(keyword)}`
 
   return fetch(url)
     .then(response => {
-      if (!response.ok) throw new Error(`에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
-      return response.json();
+      if (!response.ok) throw new Error(`지정 에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
+      return response.json().then(data => {
+        console.log(data);
+        return data
+      });
     })
     .catch(error => {
-      console.error("에러 발생:", error); // 서버 등 예측하지 못한 에러 위해
+      console.error("기타 에러:", error); // 서버 등 예측하지 못한 에러 위해
     });
 }
+
 
 
 export function getArticle(id) {
@@ -19,10 +25,43 @@ export function getArticle(id) {
 
   return fetch(url)
     .then(response => {
-      if (!response.ok) throw new Error(`에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
-      return response.json();
+      if (!response.ok) throw new Error(`지정 에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
+      return response.json().then(data => {
+        console.log(data);
+        return data;
+      });
     })
     .catch(error => {
-      // console.error("에러 발생:", error); // 서버 등 예측하지 못한 에러 위해
+      console.error("기타 에러:", error); // 서버 등 예측하지 못한 에러 위해
     });
+}
+
+
+
+export async function createArticle(ArticleTitle, ArticleContent, ArticleImage) {
+  const articleData = {
+    title: ArticleTitle,
+    content: ArticleContent,
+    image: ArticleImage
+  };
+
+  try {
+    const response = await fetch(BASE_URL, {
+      method: 'POST',
+      body: JSON.stringify(articleData),
+      headers: {
+      'Content-Type': 'application/json'
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`지정 에러: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data
+  } catch (error) {
+    console.error("기타 에러:", error);
+  }
 }
