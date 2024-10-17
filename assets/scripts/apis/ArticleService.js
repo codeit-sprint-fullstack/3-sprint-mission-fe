d4f5ce88623d302a01ed390fe2a154cb3fb31014
heonq <gkagjsrb@naver.com>
2024-10-18 00:01:50 +0900

    refactor: 리뷰 수정사항 반영

--- assets/scripts/apis/ArticleService.js
+++ assets/scripts/apis/ArticleService.js
@@ -1,32 +1,48 @@
-import api from "./common.js";
+import api from "./axiosInstance.js";
 import URLS from "../constants/url.js";
 
-const handleResponse = (apiMethod) => {
-  return apiMethod.then((response) => response.data);
+/**
+ * @param {object} body 글 조회에 필요한 정보를 담은 객체
+ * @param {number} body.page 조회할 페이지번호
+ * @param {number} body.pageSize 조회했을 때 가져올 응답값의 개수
+ * @param {string} body.keyword 글 조회에 사용할 키워드
+ * @returns {Promise<response>} 비동기로 인한 Promise로 articleList를 리턴
+ * @throws {Error} API 요청 실패 시 에러
+ */
+export const getArticleList = async (body = {}) => {
+  return api.get(URLS.articles, { body }).then((response) => response.data);
 };
 
-export const getArticleList = (page = 1, pageSize = 100, keyword = "") => {
-  const URL = `${URLS.articles}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`;
-  const data = handleResponse(api.get(URL));
-  return data;
-};
-
-export const getArticle = (id) => {
+export const getArticle = async (id) => {
   const URL = `${URLS.articles}/${id}`;
-  const data = handleResponse(api.get(URL));
-  return data;
+  return api.get(URL).then((response) => response.data);
 };
 
-export const createArticle = (body) => {
-  const data = handleResponse(api.post(URLS.articles, body));
-  return data;
+/**
+ * @param {Object} body - 글 생성에 필요한 정보를 담은 객체
+ * @param {string} body.title - 글의 제목
+ * @param {string} body.content - 글의 내용
+ * @param {string} body.image - 글에 첨부할 이미지의 URL
+ * @returns {Promise<Object>} 비동기로 인한 Promise로 생성한 글을 리턴
+ * @throws {Error} API 요청 실패 시 에러
+ */
+export const createArticle = async (body) => {
+  return api.post(URLS.articles, body).then((response) => response.data);
 };
 
-export const patchArticle = (id, body) => {
-  const data = handleResponse(api.patch(`${URLS.articles}/${id}`, body));
-  return data;
+/**
+ *@param {number} id - 수정하려는 글의 id
+ * @param {Object} body - 글 수정에 필요한 정보를 담은 객체
+ * @param {string} body.title - 수정할 제목
+ * @param {string} body.content - 수정할 내용
+ * @param {string} body.image - 수정할 이미지의 URL
+ * @returns {Promise<Object>} 비동기로 인한 Promise로 수정한 글을 리턴
+ * @throws {Error} API 요청 실패 시 에러
+ */
+export const patchArticle = async (id, body) => {
+  return api.patch(`${URLS.articles}/${id}`, body).then((response) => response.data);
 };
 
-export const deleteArticle = (id) => {
+export const deleteArticle = async (id) => {
   api.delete(`${URLS.articles}/${id}`);
 };
--- assets/scripts/apis/ProductService.js
+++ assets/scripts/apis/ProductService.js
@@ -1,9 +1,15 @@
 import URLS from "../constants/url.js";
-import api from "./common.js";
+import api from "./axiosInstance.js";
 
-export const getProductList = async (page = 1, pageSize = 100, keyword = "") => {
-  const URL = `${URLS.products}?page=${page}&pageSize=${pageSize}&keyword=${keyword}`;
-  const { data } = await api.get(URL);
+/**
+ * @param {object} body 상품목록 조회에 필요한 정보를 담은 객체
+ * @param {number} page 조회할 페이지번호
+ * @param {number} pageSize 조회했을 때 가져올 응답값의 개수
+ * @param {string} keyword 상품 조회에 사용할 키워드
+ * @returns {Promise<response>} 비동기로 인한 Promise로 productList를 리턴
+ */
+export const getProductList = async (body = {}) => {
+  const { data } = await api.get(URLS.products, { body });
   return data;
 };
 
@@ -12,11 +18,32 @@ export const getProduct = async (id) => {
   return data;
 };
 
+/**
+ * @param {Object} body - 상품 생성에 필요한 정보를 담은 객체
+ * @param {string} body.name - 상품의 이름
+ * @param {string} body.description - 상품에 대한 설명
+ * @param {number} body.price - 상품의 가격
+ * @param {string[]} body.tags - 상품의 태그가 담긴 배열
+ * @param {string[]} body.images - 상품의 이미지 경로가 담긴 배열
+ * @returns {Promise<Object>} 비동기로 인한 Promise로 생성한 상품을 리턴
+ * @throws {Error} API 요청 실패 시 에러
+ */
 export const createProduct = async (body) => {
   const { data } = await api.post(URLS.products, body);
   return data;
 };
 
+/**
+ * @param {number} id - 수정할 상품의 id
+ * @param {Object} body - 상품 수정에 필요한 정보를 담은 객체
+ * @param {string} body.name - 수정할 이름
+ * @param {string} body.description - 수정할 설명
+ * @param {number} body.price - 수정할 가격
+ * @param {string[]} body.tags - 수정할 태그가 담긴 배열
+ * @param {string[]} body.images - 수정할 이미지 경로가 담긴 배열
+ * @returns {Promise<Object>} 비동기로 인한 Promise로 수정한 상품을 리턴
+ * @throws {Error} API 요청 실패 시 에러
+ */
 export const patchProduct = async (id, body) => {
   const { data } = await api.patch(`${URLS.products}/${id}`, body);
   return data;
