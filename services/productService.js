a8b28f8781b61ca335c7520cb4f1715b390f1b81
Yousuk <yousuk88@gmail.com>
2024-10-17 17:20:42 +0900

    fix: product api를 async/await 이용하여 구현

--- services/productService.js
+++ services/productService.js
@@ -4,39 +4,43 @@ const BASE_URL = 'https://sprint-mission-api.vercel.app/products'
 
 // getProductList() : GET 메서드를 사용해 주세요.
 //  page, pageSize, keyword 쿼리 파라미터를 이용해 주세요.
-export function getProductList(page, pageSize, keyword) {
-  const url = `${BASE_URL}?page=${page}&pageSize=${pageSize}&keyword=${encodeURIComponent(keyword)}`
+export async function getProductList(page, pageSize, keyword) {
+  const url = `${BASE_URL}?page=${page}&pageSize=${pageSize}&keyword=${encodeURIComponent(keyword)}`;
 
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) throw new Error(`지정 에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
-      return response.json().then(data => {
-        console.log(data);
-        return data
-      });
-    })
-    .catch(error => {
-      console.error("기타 에러:", error); // 서버 등 예측하지 못한 에러 위해
-    });
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`지정 에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
+    }
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+
+  } catch (error) {
+    console.error("기타 에러:", error); // 서버 등 예측하지 못한 에러 위해
+  }
 }
 
 
 
 // getProduct() : GET 메서드를 사용해 주세요.
-export function getProduct(id) {
-  const url = `${BASE_URL}/${id}`
+export async function getProduct(id) {
+  const url = `${BASE_URL}/${id}`;
 
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) throw new Error(`지정 에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
-      return response.json().then(data => {
-        console.log(data);
-        return data;
-      });
-    })
-    .catch(error => {
-      console.error("기타 에러:", error); // 서버 등 예측하지 못한 에러 위해
-    });
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) throw new Error(`지정 에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+
+  } catch (error) {
+    console.error("기타 에러:", error); // 서버 등 예측하지 못한 에러 위해
+  }
 }
 
 
@@ -69,6 +73,7 @@ export async function createProduct(name, description, price, manufacturer, tags
     const data = await response.json();
     console.log(data);
     return data
+
   } catch (error) {
     console.error("기타 에러:", error);
   }
@@ -96,6 +101,7 @@ export async function patchProduct(id, updateData) {
     const data = await response.json();
     console.log(data);
     return data
+
   } catch (error) {
     console.error("기타 에러:", error.message);
   }
@@ -123,6 +129,7 @@ export async function deleteProduct(id) {
     console.log(response.status);
     console.log(response.statusText);
     return response;
+
   } catch (error) {
     console.error("기타 에러:", error.message);
   }
