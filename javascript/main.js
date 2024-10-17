f0f88686d98d170d539f1dfe75936e441c4bc42e
jonghun4 <xwhdgns@gmail.com>
2024-10-17 22:23:36 +0900

    수정했습니다

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,8 @@ console.log(res);
 const res1 = await getArticleList(1, 1, 1);
 console.log(res1);
 
-// async () => await getProductList
-// console.log(await res())
-// window.addEventListener('DOMContentLoaded', () => { console.log(async () => await getProductList(1, 1, 1))
-// })
+async () => await getProductList;
+console.log(await res());
+window.addEventListener("DOMContentLoaded", () => {
+  console.log(async () => await getProductList(1, 1, 1));
+});
