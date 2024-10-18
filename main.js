eb30c01fdc6a0ba9e2d8dd1b53d979f046e95ab9
parkjiwhan <kmo981126@naver.com>
2024-10-18 15:25:13 +0900

    article과  product 함수 import

---
+++ main.js
@@ -0,0 +1,3 @@
+import {getArticleList, getArticle, createArticle, patchArticle, deleteArticle} from './ArticleService.js'
+import {getProductList,getProduct,createProduct,patchProduct,deleteProduct} from './ProductService.js'
+
