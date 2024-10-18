221809a47ac8147e153b0aa1d38e63d217480693
parkjiwhan <kmo981126@naver.com>
2024-10-18 15:23:40 +0900

    PRODUCT PATCH 함수 작성

--- ProductService.js
+++ ProductService.js
@@ -46,3 +46,23 @@ async function createProduct(createmch) {
         console.error('에러:', error);
     }
 }
+
+async function patchProduct(productName, updateProduct) {
+    try {
+        const response = await fetch(`https://sprint-mission-api.vercel.app/products/${productName}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updateProduct)
+        });
+        if (!response.ok) {
+            throw new Error(`오류 발생: 상태 코드 ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('수정된 게시물:', data);
+        return data;
+    } catch (error) {
+        console.error('에러:', error);
+    }
+}
