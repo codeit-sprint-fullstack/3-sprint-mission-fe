39c78a9383293715233cfe33bab8b52cc4cdd4c5
KJY <139238536+dani784601@users.noreply.github.com>
2024-10-18 16:45:32 +0900

    feat: show-password 기능을 모듈로 분리

--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -1,4 +1,26 @@
-export const setVisibleInput = (e) => {
-  const pwdInput = e.target.previousElementSibling;
-  pwdInput.type = e.target.checked ? 'text' : 'password';
+const errMsgType = {
+  email: {
+    invalid: '이메일 형식이 올바르지 않습니다.',
+    empty: '이메일을 입력해주세요.'
+  },
+  password: {
+    invalid: '비밀번호는 8자 이상이어야 합니다.',
+    empty: '비밀번호를 입력해주세요.'
+  }
+}
+
+export const toggleInputVisibility = (target) => {
+  const pwdInput = target.previousElementSibling;
+  pwdInput.type = target.checked ? 'text' : 'password';
+}
+
+/**
+ *
+ * @param { HTMLInputElement } target
+ */
+export const setValidationMsg = (target) => {
+  const errMsg = target.parentElement.querySelector('.form-error-msg');
+  const { valid } = target.validity;
+  console.log(valid)
+
 }
