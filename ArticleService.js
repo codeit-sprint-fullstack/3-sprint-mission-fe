e705e38a48612aff9a02cb2109725abf1fabf08a
parkjiwhan <kmo981126@naver.com>
2024-10-18 15:18:56 +0900

    Article POST 보내기 작성

--- ArticleService.js
+++ ArticleService.js
@@ -32,3 +32,27 @@ function getArticle(articleId) {
         });
 }
 
+function createArticle(articlePost) {
+    fetch('https://sprint-mission-api.vercel.app/articles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(articlePost)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`오류 발생: 상태 코드 ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+        .catch(error => {
+            console.error('에러:', error);
+        });
+}
+
+
