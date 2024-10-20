0578350212f31b32cc4c9f613ab682058d137fb5
jonghun4 <xwhdgns@gmail.com>
2024-10-20 23:08:26 +0900

    변경

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,11 @@
-import { getArticleList } from "./ArticleService.js";
-import { getProductList } from "./ProductService.js";
+import { getArticleList, getArticle, createArticle, patchArticle, deleteArticle } from "./ArticleService.js";
+import { getProductList, getProduct, createProduct, patchProduct, deleteProduct } from "./ProductService.js";
 
-const res = await getProductList(1, 1, 1);
-console.log(res);
-const res1 = await getArticleList(1, 1, 1);
-console.log(res1);
+async () => await getArticleList;
+console.log(await res());
+window.addEventListener("DOMContentLoaded", () => {
+  console.log(async () => await getArticleList(1, 1, 1));
+});
 
 async () => await getProductList;
 console.log(await res());
