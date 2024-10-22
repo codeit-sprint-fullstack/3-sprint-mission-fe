1d8101fe5e191886f2900fbe4b73b3bdedba41f8
heonq <gkagjsrb@naver.com>
2024-10-22 09:32:51 +0900

    fix: styled-components import 수정

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,4 +1,4 @@
-import createGlobalStyle from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import Pretendard from "../assets/fonts/PretendardVariable.woff2";
 
@@ -6,8 +6,12 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
   * {
     box-sizing:border-box;
-    font-size:62.5%;
   }
+
+  html {
+    font-size:62.5%;
+  }  
+
   body {
     font-family:${Pretendard};
     font-size:16rem;
