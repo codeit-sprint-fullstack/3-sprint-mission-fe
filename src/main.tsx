c7773bb48555cbef8fd42a051299853bbc3e7ce1
Daewony <alexnori.com@gmail.com>
2024-10-22 10:56:19 +0900

    style: 로고 및 파비콘 관련 이미지 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './reset.css';
+import './assets/fonts/fonts.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
