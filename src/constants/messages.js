4604b6720fb9a2de1482e7ee7641b92792b13243
heonq <gkagjsrb@naver.com>
2024-10-22 11:16:55 +0900

    feat: api 기능 구현

---
+++ src/constants/messages.js
@@ -0,0 +1,27 @@
+import { VALIDATION_VALUES } from "./validationValues.js";
+
+export const ERROR_MESSAGES = {
+  inputEmail: "이메일을 입력해주세요.",
+  invalidEmail: "잘못된 이메일 형식입니다",
+  inputPassword: "비밀번호를 입력해주세요.",
+  inputValidLengthPassword: `비밀번호를 ${VALIDATION_VALUES.passwordLength}자 이상 입력해주세요.`,
+  passwordNotMatch: "비밀번호가 일치하지 않습니다.",
+  emailAlreadyInUse: "사용 중인 이메일입니다",
+  inputNickname: "닉네임을 입력해주세요.",
+};
+
+export const DOCUMENT_ERROR_MESSAGES = {
+  failCreatingDocument: "문서를 생성하는데 실패했습니다 : ",
+  failGettingDocument: "문서를 불러오는데 실패했습니다 : ",
+  failGettingDocumentList: "문서 목록을 불러오는데 실패했습니다 : ",
+  failPatchingDocument: "문서를 수정하는데 실패했습니다 : ",
+  failDeletingDocument: "문서를 삭제하는데 실패했습니다 : ",
+  succededDeletingDocument: "성공적으로 문서를 삭제했습니다.",
+};
+
+export const HTTP_ERROR_MESSAGE = {
+  404: "문서를 찾을 수 없습니다.",
+  400: "잘못된 값이 입력되었습니다.",
+  requestError: "서버로부터 응답이 없습니다.",
+  else: "예기치 않은 오류가 발생했습니다.",
+};
---
+++ src/constants/url.js
@@ -0,0 +1,7 @@
+const URLS = {
+  baseUrl: "https://sprint-mission-api.vercel.app",
+  articles: "articles",
+  products: "products",
+};
+
+export default URLS;
