d43e6e547b5e5b9fa3401a55a958c6499765d054
soohwanpak <qoiuytnghks4@gmail.com>
2024-10-26 02:24:43 +0900

    1026수정

--- react-psh-sprint5/src/main.jsx
+++ react-psh-sprint5/src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './css/reset.css'
 
 createRoot(document.getElementById('root')).render(
     <App />
