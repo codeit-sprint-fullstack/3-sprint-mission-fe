7ca441881099f9f0cdb704cc2a9ef25550f5db71
Jinhan <wlsgks333@naver.com>
2024-10-27 20:26:32 +0900

    판매 중인 상품 목록, 화면 켜지자마자 렌더링 구현 완료

--- sprint5/src/components/Main.js
+++ sprint5/src/components/Main.js
@@ -4,24 +4,38 @@ import './Main.css';
 import arrowImg from "../img/icons/menu.png";
 import leftArrow from "../img/icons/left.png";
 import rightArrow from "../img/icons/right.png";
-import BestProdsList from "./BestProdsList";
-import ProdsList from "./ProdsList"
+import BestProdsList from "./BestProdsList.js";
+import ProdsList from "./ProdsList.js";
 
 const BEST_PRODS_LIST = 'page=1&pageSize=4&orderBy=favorite';
+const PRODS_LIST = 'page=1&pageSize=10&orderBy=recent';
 
 function Main() {
   // Use State
   const [bestProds, setBestProds] = useState([]); // 베스트 상품
+  const [prodsList, setProdsList] = useState([]); // 판매 중인 상품
 
   // 화면이 켜지자마자 렌더링
   const loadHandle = async () => {
     const bestProducts = await getProducts(BEST_PRODS_LIST);
-    setBestProds(bestProducts.list)
+    const products = await getProducts(PRODS_LIST);
+    setBestProds(bestProducts.list);
+    setProdsList(products.list);
   }
 
   useEffect(() => {
     loadHandle()
   }, [])
+
+  // 판매 중인 상품 렌더링
+  const ProductSort = ({ sort }) => {
+
+    return (
+      sort.map((prod) => {
+        return <ProdsList price={prod.price} images={prod.images} name={prod.name} favoriteCount={prod.favoriteCount} />
+      })
+    )
+  }
 
   return (
     <main>
@@ -47,16 +61,7 @@ function Main() {
             </div>
           </div>
           <div id='componentBox'>
-            <ProdsList />
-            <ProdsList />
-            <ProdsList />
-            <ProdsList />
-            <ProdsList />
-            <ProdsList />
-            <ProdsList />
-            <ProdsList />
-            <ProdsList />
-            <ProdsList />
+            <ProductSort sort={prodsList} />
           </div>
           <div id='pageNumBox'>
             <button>
--- sprint5/src/components/ProdsList.js
+++ sprint5/src/components/ProdsList.js
@@ -1,15 +1,15 @@
 import heartImg from '../img/icons/heart.png';
 import './ProductsInfo.css';
 
-function ProdsList() {
+function ProdsList({price, images, name, favoriteCount}) {
   
   return (
-    <div id="products">
-      <img src id='productsImg'/>
-      <div className='prodName'>상품명</div>
-      <div className='prodPrice'>가격</div>
+    <div id='products'>
+      <img id='productsImg' src={images} />
+      <div className='prodName'>{name}</div>
+      <div className='prodPrice'>{price}원</div>
       <img src={heartImg} className='heart' />
-      <span className='heartNum'>점수</span>
+      <span className='heartNum'>{favoriteCount}</span>
     </div>
   )
 }
