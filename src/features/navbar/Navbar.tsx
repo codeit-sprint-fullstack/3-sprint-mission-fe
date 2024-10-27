438c6da4c14cc6649d052b5b99fba3e9be4b7b2e
Daewony <alexnori.com@gmail.com>
2024-10-27 23:33:39 +0900

    feat: Navbar 컴포넌트 추가

---
+++ src/features/navbar/Navbar.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import PANDA_MARKET_LOGO_IMAGE from '../../assets/images/logo/panda-market-logo.svg';
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return (
+    <header>
+      <Nav aria-label="메인 내비게이션">
+        <NavContainer>
+          <MenuContainer>
+            <a href="/">
+              <img src={PANDA_MARKET_LOGO_IMAGE} alt="판다마켓 로고 아이콘" />
+            </a>
+            <a href="/">자유게시판</a>
+            <a href="/">중고마켓</a>
+          </MenuContainer>
+          <LoginButton aria-label="로그인 버튼">로그인</LoginButton>
+        </NavContainer>
+      </Nav>
+    </header>
+  );
+};
+
+export default Navbar;
+
+const Nav = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  background: white;
+  z-index: 1;
+  max-height: 70px;
+  border-bottom: 1px solid var(--navbar-border-color);
+`;
+
+const NavContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 20rem;
+
+  @media (min-width: 744px) and (max-width: 1199px) {
+    padding: 1rem 2.4rem;
+  }
+
+  @media (max-width: 743px) {
+    padding: 1rem 1.6rem;
+  }
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  font-weight: bold;
+
+  a {
+    color: var(--nav-menu-gray-font-color);
+    font-size: 1.6rem;
+  }
+`;
+
+const LoginButton = styled.button`
+  min-width: 12.8rem;
+  height: 4.8rem;
+  border: none;
+  border-radius: 0.8rem;
+  background-color: var(--primary-blue-color);
+  color: white;
+  padding: 1.2rem 2.3rem;
+  font-weight: 600;
+  font-size: 1.6rem;
+  white-space: nowrap;
+`;
