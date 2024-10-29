c942da3c83aa76fae4b23141eddcf39a6e5e7877
Kimhyunmook <gusanr4200@naver.com>
2024-10-29 16:09:53 +0900

    중고마켓 추가

--- market/src/main.jsx
+++ market/src/main.jsx
@@ -3,6 +3,7 @@ import Market from "./pages/market";
 import Header from "./component/header";
 import Footer from "./component/footer";
 import Home from "./pages/home";
+import UsedMarket from "./pages/usedMarket";
 export default function Main() {
   let init = {
     paddingTop: "70px",
@@ -14,6 +15,7 @@ export default function Main() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/marekt" element={<Market />} />
+          <Route path="/usedmarket" element={<UsedMarket />} />
         </Routes>
         <Footer />
       </div>
