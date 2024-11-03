36e439f8f53db0e33738cea07bd5e88beb3773b2
Yousuk <yousuk88@gmail.com>
2024-11-03 22:54:34 +0900

    fix: Nav 뒤 배경 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,25 +8,28 @@ function Nav() {
   const isLoggedIn = false; // true 또는 false로 각각 바꿔보며 확인
 
   return (
-    <div className={styles.nav}>
-      <div className={styles.gnb}>
-        <Link to="/"><div className={styles.logo}>
-          <img src='/static/images/logo.png' alt="로고" className={styles.logoImg} />
-          <div className={styles.logoTitle}>판다마켓</div>
-        </div></Link>
-        <ul className={styles.ul}>
-          <Link to="/community"><li className={`${styles.li} ${location.pathname === '/community' ? styles.active : ''}`}>자유게시판</li></Link>
-          <Link to="/items"><li className={`${styles.li} ${location.pathname === '/items' ? styles.active : ''}`}>중고마켓</li></Link>
-        </ul>
-        {isLoggedIn ? (
-          <div className={styles.avatar}>
-            <img src='/static/images/user_profile.png' alt="유저 프로필" />
-          </div>
-        ) : (
-          <Link to="/login"><div className={styles.login}>로그인</div></Link>
-        )}
+    <>
+      <div className={styles.nav}>
+        <div className={styles.gnb}>
+          <Link to="/"><div className={styles.logo}>
+            <img src='/static/images/logo.png' alt="로고" className={styles.logoImg} />
+            <div className={styles.logoTitle}>판다마켓</div>
+          </div></Link>
+          <ul className={styles.ul}>
+            <Link to="/community"><li className={`${styles.li} ${location.pathname === '/community' ? styles.active : ''}`}>자유게시판</li></Link>
+            <Link to="/items"><li className={`${styles.li} ${location.pathname === '/items' ? styles.active : ''}`}>중고마켓</li></Link>
+          </ul>
+          {isLoggedIn ? (
+            <div className={styles.avatar}>
+              <img src='/static/images/user_profile.png' alt="유저 프로필" />
+            </div>
+          ) : (
+            <Link to="/login"><div className={styles.login}>로그인</div></Link>
+          )}
+        </div>
       </div>
-    </div>
+      <div style={{ height: '70px' }}></div >{/* nav 뒤 채우기 */}
+    </>
   );
 }
 
