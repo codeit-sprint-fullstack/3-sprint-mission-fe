15cf2ac6c21ac93564f1178ec0ff34dabf36317d
Yousuk <yousuk88@gmail.com>
2024-11-04 00:31:49 +0900

    add: 랜딩페이지 SearchSection

--- src/components/Main/SearchSection.js
+++ src/components/Main/SearchSection.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './SearchSection.module.css'
 
 function SearchSection() {
   return (
-    <div className="coming-soon">SearchSection</div>
+    <section className={styles.search_section}>
+      <div className={styles.search_box}>
+        <div className={styles.search_p}>
+          <span>Search</span>
+          <p>구매를 원하는<br />상품을 검색하세요</p>
+          <div>구매하고 싶은 물품은 검색해서<br />쉽게 찾아보세요</div>
+        </div>
+        <img src="static/images/Img_home_02.png" alt="search product" />
+      </div>
+    </section>
   )
 }
 
