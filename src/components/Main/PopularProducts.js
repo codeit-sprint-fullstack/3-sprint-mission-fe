1bf810e639787817d8b560ba7ff4c8b021ab827a
Yousuk <yousuk88@gmail.com>
2024-11-04 00:24:16 +0900

    add: 랜딩페이지 PopularSection

--- src/components/Main/PopularProducts.js
+++ src/components/Main/PopularProducts.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './PopularProducts.module.css'
 
 function PopularProducts() {
   return (
-    <div className="coming-soon">PopularProducts</div>
+    <section className={styles.popular_products}>
+      <div className={styles.popular_box}>
+        <img src="/static/images/Img_home_01.png" alt="popular product" />
+        <div className={styles.popular_p}>
+          <span>Hot item</span>
+          <p>인기 상품을<br />확인해 보세요</p>
+          <div>가장 HOT한 중고거래 물품을<br />판다 마켓에서 확인해 보세요</div>
+        </div>
+      </div>
+    </section>
   )
 }
 
