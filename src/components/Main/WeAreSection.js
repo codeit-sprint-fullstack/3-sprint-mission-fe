45dc50bc5b6e391622f5ae354a6bce7652e438b8
Yousuk <yousuk88@gmail.com>
2024-11-04 00:56:26 +0900

    add: 랜딩페이지 WeAreSection

--- src/components/Main/WeAreSection.js
+++ src/components/Main/WeAreSection.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from './WeAreSection.module.css'
 
 function WeAreSection() {
   return (
-    <div className="coming-soon">WeAreSection</div>
+    <section className={styles.we_are}>
+      <div className={styles.we_are_box}>
+        <div className={styles.we_are_p}>믿을 수 있는<br />판다 마켓 중고거래</div>
+        <img src="/static/images/Img_home_bottom.png" alt="panda bottom" className={styles.sc5_img5} />
+      </div>
+    </section >
   )
 }
 
