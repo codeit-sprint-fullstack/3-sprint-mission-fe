99113c44ae279b16788e50c59ff24b64fcbf6549
Yousuk <yousuk88@gmail.com>
2024-11-04 00:15:36 +0900

    add: 랜딩페이지 HeroSection

--- src/components/Main/HeroSection.js
+++ src/components/Main/HeroSection.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styles from './HeroSection.module.css'
 
 function HeroSection() {
   return (
-    <div className="coming-soon">HeroSection</div>
+    <section className={styles.hero_section}>
+      <div className={styles.hero_box}>
+        <div className={styles.hero_p}>
+          <p>일상의 모든 물건을<br />거래해 보세요</p>
+          <Link to="/items">
+            <div className={styles.go_button}>구경하러 가기</div>
+          </Link>
+        </div>
+        <img src="/static/images/Img_home_top.png" alt="look around" className={styles.hero_img1} />
+      </div>
+    </section>
   )
 }
 
