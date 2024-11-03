f993cc7b919c70bef71dc151c24bd3aa2e13ce86
Yousuk <yousuk88@gmail.com>
2024-11-04 00:38:50 +0900

    add: 랜딩페이지 RegisterProduct

--- src/components/Main/RegisterProduct.js
+++ src/components/Main/RegisterProduct.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from './RegisterProduct.module.css'
 
 function RegisterProduct() {
   return (
-    <div className="coming-soon">RegisterProduct</div>
+    <section className={styles.register_product}>
+      <div className={styles.register_box}>
+        <img src="/static/images/Img_home_03.png" alt="register product" />
+        <div className={styles.register_p}>
+          <span>Register</span>
+          <p>판매를 원하는<br />상품을 등록하세요</p>
+          <div>어떤 물건이든 판매하고 싶은 상품을<br />쉽게 등록하세요</div>
+        </div>
+      </div>
+    </section>
   )
 }
 
