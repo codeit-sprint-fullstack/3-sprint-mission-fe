cfb928175f74f1b026a34e7370e36d1d1b5fa742
Jinhan <wlsgks333@naver.com>
2024-11-05 18:21:37 +0900

    RegisterItemPage, 상품 소개 validation 완료

--- sprint6/src/pages/RegisterItemPage/components/ProdDescriptionInputBox/index.jsx
+++ sprint6/src/pages/RegisterItemPage/components/ProdDescriptionInputBox/index.jsx
@@ -1,4 +1,24 @@
+import React, { useState } from "react";
+import "./index.css";
+
 const ProdDescriptionInputBox = ({ inputValue, onChange }) => {
+  const [descriptionValidation, setDescriptionValidation] = useState(false);
+  const [emptyDescriptionInput, setEmptyDescriptionInput] =
+    useState("10자 이상 입력해주세요");
+
+  const handleDescriptionInputBlur = (e) => {
+    if (e.target.value.length < 10) {
+      setEmptyDescriptionInput("10자 이상 입력해주세요");
+      setDescriptionValidation(true);
+    }
+    if (e.target.value.length >= 10 && e.target)
+      setDescriptionValidation(false);
+    if (e.target.value.length === 0 && e.target) {
+      setEmptyDescriptionInput("상품 소개를 입력해주세요");
+      setDescriptionValidation(!descriptionValidation);
+    }
+  };
+
   return (
     <div id="prodIntroBox" className="mainWidth">
       <h1>상품 소개</h1>
@@ -8,8 +28,16 @@ const ProdDescriptionInputBox = ({ inputValue, onChange }) => {
         onChange={onChange}
         className="inputBox"
         type="text"
+        onBlur={handleDescriptionInputBlur}
         placeholder="상품 소개를 입력해주세요"
+        id={descriptionValidation ? "redBorder" : ""}
       />
+      <span
+        style={descriptionValidation ? {} : { display: "none" }}
+        className="productDescriptionValidation"
+      >
+        {emptyDescriptionInput}
+      </span>
     </div>
   );
 };
--- sprint6/src/pages/RegisterItemPage/components/ProdNameInputBox/index.jsx
+++ sprint6/src/pages/RegisterItemPage/components/ProdNameInputBox/index.jsx
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import "./index.css";
 
 const ProdNameInputBox = ({ inputValue, onChange }) => {
-const [nameValidation, setNameValidation] = useState(false);
+  const [nameValidation, setNameValidation] = useState(false);
+  const [emptyNameInput, setEmptyNameInput] = useState('10자 이내로 입력해주세요');
 
   const handleNameInputBlur = (e) => {
     if (e.target.value.length >= 10) {
+      setEmptyNameInput('10자 이내로 입력해주세요')
       setNameValidation(true);
-    } else if (e.target.value.length < 10 && e.target) {
-      setNameValidation(false);
     }
-  }
-    
+    if (e.target.value.length < 10 && e.target) setNameValidation(false);
+    if (e.target.value.length === 0 && e.target) {
+      setNameValidation(!nameValidation);
+      setEmptyNameInput('상품명을 입력주세요');
+    }
+  };
+
   return (
     <div id="prodNmaeBox" className="mainWidth">
       <h1>상품명</h1>
@@ -25,7 +30,12 @@ const [nameValidation, setNameValidation] = useState(false);
         onBlur={handleNameInputBlur}
         id={nameValidation ? "redBorder" : ""}
       />
-      <span style={nameValidation ? {} : { display: "none" }} className="productNameValidation">10자 이내로 입력해주세요</span>
+      <div
+        style={nameValidation ? {} : { display: "none" }}
+        className="productNameValidation"
+      >
+        {emptyNameInput}
+      </div>
     </div>
   );
 };
