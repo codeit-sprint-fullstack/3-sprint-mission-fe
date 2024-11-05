ae1e1f3d940d400a3c5d2afcd3bce9e9daaaa857
Jinhan <wlsgks333@naver.com>
2024-11-05 16:40:52 +0900

    RegisterItemPage, 상품명 심화 요구 사항 완료

--- sprint6/src/pages/RegisterItemPage/components/ProdNameInputBox/index.jsx
+++ sprint6/src/pages/RegisterItemPage/components/ProdNameInputBox/index.jsx
@@ -1,4 +1,17 @@
+import React, { useState } from "react";
+import "./index.css";
+
 const ProdNameInputBox = ({ inputValue, onChange }) => {
+const [nameValidation, setNameValidation] = useState(false);
+
+  const handleNameInputBlur = (e) => {
+    if (e.target.value.length >= 10) {
+      setNameValidation(true);
+    } else if (e.target.value.length < 10 && e.target) {
+      setNameValidation(false);
+    }
+  }
+    
   return (
     <div id="prodNmaeBox" className="mainWidth">
       <h1>상품명</h1>
@@ -9,7 +22,10 @@ const ProdNameInputBox = ({ inputValue, onChange }) => {
         className="inputBox"
         type="text"
         placeholder="상품명을 입력해주세요"
+        onBlur={handleNameInputBlur}
+        id={nameValidation ? "redBorder" : ""}
       />
+      <span style={nameValidation ? {} : { display: "none" }} className="productNameValidation">10자 이내로 입력해주세요</span>
     </div>
   );
 };
