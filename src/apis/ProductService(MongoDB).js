432323c2396406e9d9c7deae1b4da4a28b17980c
Yousuk <yousuk88@gmail.com>
2024-11-06 14:27:15 +0900

    add: 로컬 서버로부터 호출 api

---
+++ src/apis/ProductService(MongoDB).js
@@ -0,0 +1,27 @@
+const BASE_URL = 'http://localhost:8000/api/products'
+
+export async function getProductList(page, pageSize, orderBy, keyword) {
+  const params = new URLSearchParams({
+    page,
+    pageSize,
+    orderBy,
+    keyword
+  });
+
+  const url = `${BASE_URL}?${params}`
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`지정 에러: ${response.status} - ${response.statusText}`); // 내가 지정한 오류 핸들링
+    }
+
+    const data = await response.json();
+    // console.log(data);
+    return data;
+
+  } catch (error) {
+    console.error("기타 에러:", error); // 서버 등 예측하지 못한 에러 위해
+  }
+}
