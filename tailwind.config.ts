192fb33ad01eeeded4be0e27081b9a03140ab8ec
Daewony <alexnori.com@gmail.com>
2024-12-02 17:25:34 +0900

    style: 테일윈드 글로벌 색상 변수 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        blue: {
+          DEFAULT: 'var(--blue)',
+          text: 'var(--blue-text)',
+        },
+        gray: {
+          DEFAULT: 'var(--gray)',
+          light: 'var(--gray-light)',
+          dart: 'var(--gray-dark)',
+          border: 'var(--gray-border)',
+          bg_card: 'var(--gray-bg-card)',
+          bg_list: 'var(--gray-bg-list)',
+          heart_number: 'var(--gray-heart-number)',
+          footer_text: 'var(--gray-footer-text)',
+        },
+        black: {
+          DEFAULT: 'var(--black)',
+        },
       },
     },
   },
