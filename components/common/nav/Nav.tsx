cbb2b43a6d91a17b886f68937c939e2695f914b8
Daewony <alexnori.com@gmail.com>
2024-12-02 17:26:44 +0900

    feat: layout 파일에 nav, footer 공통 컴포넌트 추가

--- components/common/nav/Nav.tsx
+++ components/common/nav/Nav.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image';
+import Logo from '@/public/images/logo/logo.svg';
+import Link from 'next/link';
+
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div>
+        <Image src={Logo} alt="로고 이미지" width={153} height={51} />
+        <Link href={'/board'}>자유 게시판</Link>
+        <Link href={'/market'}>중고마켓</Link>
+      </div>
+      <button></button>
+    </nav>
+  );
 };
 
 export default Nav;
