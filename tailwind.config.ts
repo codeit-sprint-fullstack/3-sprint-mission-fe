790c1ae0a419bcf5227df267c5f548d2cac791a2
heonq <gkagjsrb@naver.com>
2024-12-02 17:39:49 +0900

    style: 스타일 변수 선언

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,49 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+
+        primary: 'var(--primary-blue)',
+
+        text: {
+          black: {
+            primary: 'var(--text-primary-black)',
+            secondary: 'var(--text-secondary-black)',
+          },
+          gray: {
+            primary: 'var(--text-primary-gray)',
+            secondary: 'var(--text-secondary-gray)',
+          },
+          white: 'var(--text-primary-white)',
+        },
+        bg: {
+          input: 'var(--input-background)',
+          article: {
+            best: 'var(--best-article-background)',
+            normal: 'var(--normal-article-background)',
+          },
+          badge: {
+            best: 'var(--best-badge-background)',
+          },
+          button: {
+            active: 'var(--button-active-background)',
+            disabled: 'var(--button-disabled-background)',
+          },
+        },
+
+        border: {
+          gnb: 'var(--gnb-border)',
+          normalArticle: 'var(--normal-article-border)',
+        },
       },
     },
   },
