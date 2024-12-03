62bb3ab2d52f52c779271b66c4bfeb925d9ae6ad
Jinhan <wlsgks333@naver.com>
2024-12-04 02:27:20 +0900

    style: 코드 포맷, 스타일 정리

--- sprint8/components/CommunityFeed/BestArticles.jsx
+++ sprint8/components/CommunityFeed/BestArticles.jsx
@@ -1,43 +1,48 @@
+import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/components/CommunityFeed/BestArticles.module.css";
 
 function BestArticles() {
   return (
-    <div className={styles.bestArticlesBody}>
-      <div className={styles.bestArticle}>
-        <div className={styles.bestMark}>
-          <Image
-            src="/images/icons/ic_medal.png"
-            alt="베스트 게시글 메달 이미지"
-            width={16}
-            height={16}
-          />
-          <span className={styles.bestMarkText}>Best</span>
-        </div>
-        <div className={styles.bestArticleContent}>
-          <h2 className={styles.bestArticleTitle}>맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?</h2>
-          <Image
-            src="/images/default/FE_default_Img.png"
-            alt="베스트 게시글 이미지"
-            width={72}
-            height={72}
-          />
-        </div>
-        <div className={styles.articleInfoBox}>
-          <div className={styles.articleInfoLeft}>
-            <span className={styles.nickname}>총명한판다</span>
+    <Link
+      href="/ItemDetail/1"
+    >
+      <div className={styles.bestArticlesBody}>
+        <div className={styles.bestArticle}>
+          <div className={styles.bestMark}>
             <Image
-              src="/images/icons/heart.png"
-              alt="하트 이미지"
-              width={10}
-              height={10}
+              src="/images/icons/ic_medal.png"
+              alt="베스트 게시글 메달 이미지"
+              width={16}
+              height={16}
             />
-            <div className={styles.articleLikeNum}>9999+</div>
+            <span className={styles.bestMarkText}>Best</span>
           </div>
-          <div className={styles.articleCreateDate}>2024. 12. 16</div>
+          <div className={styles.bestArticleContent}>
+            <h2 className={styles.bestArticleTitle}>맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?</h2>
+            <Image
+              src="/images/default/FE_default_Img.png"
+              alt="베스트 게시글 이미지"
+              width={72}
+              height={72}
+            />
+          </div>
+          <div className={styles.articleInfoBox}>
+            <div className={styles.articleInfoLeft}>
+              <span className={styles.nickname}>총명한판다</span>
+              <Image
+                src="/images/icons/heart.png"
+                alt="하트 이미지"
+                width={10}
+                height={10}
+              />
+              <div className={styles.articleLikeNum}>9999+</div>
+            </div>
+            <div className={styles.articleCreateDate}>2024. 12. 16</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
