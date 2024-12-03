5cf1f72532799e82a185d5dd9a88e9a11f52f0e0
Jinhan <wlsgks333@naver.com>
2024-12-03 17:09:28 +0900

    styles: Header.jsx css 및 이미지 버그 수정

--- sprint8/components/ui/Header.jsx
+++ sprint8/components/ui/Header.jsx
@@ -5,51 +5,59 @@ import styles from "@/styles/ui/Header.module.css";
 const getLinkStyle = ({ isActive }) => {
   return {
     color: isActive ? "#FFFFFF" : "#747474",
-    
   };
 };
 
 function Header() {
   return (
     <header>
-    <nav className={styles.header}>
-      <div className={styles.headerContent}>
+      <nav className={styles.header}>
+        <div className={styles.headerContent}>
           <Link href="/index">
-            <div className={styles.pandaLogoImg} >
+            <div
+              className={styles.pandaLogoImg}
+              style={{
+                position: "relative",
+              }}
+            >
               <Image
                 src="/images/logo/panda-market-logo.png"
-                width={153}
-                height={51}
-                alt='판다마켓 로고' />
+                fill
+                alt="판다마켓 로고"
+                style={{
+                  objectFit: "cover",
+                }}
+              />
             </div>
             <div className={styles.pandaLogoText}>
               <Image
-                src="/images/logo/panda-text-logo.png"
+                src="/images/logo/panda-text-log.png"
                 width={113}
                 height={41}
-                alt='판다마켓 로고(글씨)' />
+                alt="판다마켓 로고(글씨)"
+              />
             </div>
-        </Link>
+          </Link>
 
-        <div className={styles.headerTextBox}>
-          <div className={styles.headerText}>
-            <Link href="/CommunityFeed" style={{getLinkStyle}}>
-              자유게시판
-            </Link>
+          <div className={styles.headerTextBox}>
+            <div className={styles.headerText}>
+              <Link href="/CommunityFeed" style={{ getLinkStyle }}>
+                자유게시판
+              </Link>
+            </div>
+            <div className={styles.headerText}>
+              <Link href="/Items" style={{ getLinkStyle }}>
+                중고마켓
+              </Link>
+            </div>
           </div>
-          <div className={styles.headerText}>
-            <Link href="/Items" style={{getLinkStyle}}>
-              중고마켓
-            </Link>
-          </div>
+
+          <button className={styles.loginButtn}>
+            <Link href="/Login">로그인</Link>
+          </button>
         </div>
-
-        <button className={styles.loginButtn}>
-          <Link href="/Login">로그인</Link>
-        </button>
-      </div>
-    </nav>
-  </header>
+      </nav>
+    </header>
   );
 }
 
