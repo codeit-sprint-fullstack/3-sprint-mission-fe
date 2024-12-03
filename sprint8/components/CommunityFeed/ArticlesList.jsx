901002e150c04258fc06675bbe54ee5336b521d0
Jinhan <wlsgks333@naver.com>
2024-12-04 05:59:58 +0900

    feat: ArticlesList.jsx 컴포넌트 기본 디자인 적용 완료

---
+++ sprint8/components/CommunityFeed/ArticlesList.jsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/components/CommunityFeed/Articles.module.css";
+
+function ArticlesList() {
+  return (
+    <div style={{
+      width: "1200px",
+    }}>
+      <div className={styles.searchSortBox}>
+        {/* input 검색창 컴포넌트*/}
+        {/* dropDown 정렬 컴포넌트*/}
+      </div>
+      <div className={styles.articlesListBody}>
+        <Link
+          href="/ArticleDetail/1"
+        >
+          <div className={styles.article}>
+            <div className={styles.articleContent}>
+              <h2 className={styles.articleTitle}>맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?</h2>
+              <Image
+                src="/images/default/FE_default_Img.png"
+                alt="베스트 게시글 이미지"
+                width={72}
+                height={72}
+              />
+            </div>
+            <div className={styles.articleInfoBox}>
+              <div className={styles.articleInfoLeft}>
+                <Image
+                  src="/images/icons/ic_profile.png"
+                  alt="프로필 이미지"
+                  width={24}
+                  height={24}
+                />
+                <span className={styles.nickname}>총명한판다</span>
+                <div className={styles.articleCreateDate}>2024. 12. 16</div>
+              </div>
+              <div className={styles.articleInfoRight}>
+                <Image
+                  src="/images/icons/heart.png"
+                  alt="하트 이미지"
+                  width={13.75}
+                  height={13}
+                />
+                <div className={styles.articleLikeNum}>9999+</div>
+              </div>
+            </div>
+          </div>
+        </Link >
+      </div>
+    </div>
+  )
+}
+
+export default ArticlesList;
