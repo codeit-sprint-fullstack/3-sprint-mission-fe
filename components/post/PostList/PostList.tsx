7e1f5f82ff0116e600c20f03d64a478e387299d2
Daewony <alexnori.com@gmail.com>
2024-12-03 16:22:53 +0900

    feat: 게시글 검색 및 게시글 카드 컴포넌트 UI 구현

--- components/post/PostList/PostList.tsx
+++ components/post/PostList/PostList.tsx
@@ -1,18 +1,15 @@
-import CommonBtn from "@/components/common/button/CommonBtn";
-import PostListTitle from "./PostListTitle";
 import Image from "next/image";
-import profileIcon from "@/public/icons/ic_profile.png";
-import heartIcon from "@/public/icons/ic_heart.svg";
+
+import PostListTitle from "./PostListTitle";
+import PostListCard from "./PostListCard";
+import CommonBtn from "@/components/common/button/CommonBtn";
+
 import searchIcon from "@/public/icons/ic_search.svg";
 import sortButton from "@/public/images/btn_sort.png";
 
 const PostList = () => {
-  const content = "맥북 16인치 16기가 1테라 정도 사양이면 얼마에 팔아야하나요?";
-  const userName = "총명한판다";
-  const updateAt = "2024. 04. 16";
-  const likeCount = "9999+";
   return (
-    <section className="max- box-border">
+    <section className="box-border">
       {/* 게시글 헤더 */}
       <div className="mb-6 flex items-center justify-between">
         <PostListTitle />
@@ -51,27 +48,7 @@ const PostList = () => {
 
       {/* 게시글 리스트 */}
       <ul>
-        <li className="mb-6 h-[140px] border-b-[1px] border-gray-footer_text bg-gray-bg_list pb-6">
-          <div className="h-[88px]">
-            <p className="text-xl font-semibold text-black">{content}</p>
-          </div>
-          <div className="flex justify-between">
-            <div className="flex gap-2 text-sm">
-              <Image
-                src={profileIcon}
-                alt="profile image"
-                height={24}
-                width={24}
-              />
-              <span className="text-gray-dark">{userName}</span>
-              <span className="text-gray">{updateAt}</span>
-            </div>
-            <div className="flex gap-2 text-base">
-              <Image src={heartIcon} alt="heart" height={24} width={24} />
-              <span className="text-gray-heart_number">{likeCount}</span>
-            </div>
-          </div>
-        </li>
+        <PostListCard />
       </ul>
     </section>
   );
