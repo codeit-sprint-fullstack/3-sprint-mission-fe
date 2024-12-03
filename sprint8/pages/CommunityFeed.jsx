1ca5fa4ef1119984d1ae2bedf79670b014229a63
Jinhan <wlsgks333@naver.com>
2024-12-03 21:36:58 +0900

    chore: BestArticles.jsx 컴포넌트 적용

--- sprint8/pages/CommunityFeed.jsx
+++ sprint8/pages/CommunityFeed.jsx
@@ -1,11 +1,12 @@
 import styles from "@/styles/pages/CommunityFeed.module.css"
+import BestArticles from "@/components/CommunityFeed/BestArticles"
 
 function CommunityFeed() {
   return (
     <div className={styles.communityFeedBody}>
       <div className={styles.communityFeedMain}>
       <h1 className={styles.communityFeedTitle}>베스트 게시글</h1>
-      {/* 베스트 게시글 컴포넌트 */}
+        <BestArticles />
       <div className={styles.articlePostHeader}>
         <h1 className={styles.communityFeedTitle}>게시글</h1>
         <button className={styles.toArticlePostButton}>글쓰기</button>
