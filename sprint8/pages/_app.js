303827357ecdb8ac45d40782670ea2e8042f9691
Jinhan <wlsgks333@naver.com>
2024-12-03 15:32:15 +0900

    chore: _app.js 파일 Nav를 Header로 변경

--- sprint8/pages/_app.js
+++ sprint8/pages/_app.js
@@ -1,15 +1,15 @@
-import Nav from "@/components/ui/Header.jsx";
+import Header from "@/components/ui/Header.jsx";
 import "@/styles/globals.css";
 
 /**파라미터 Component, pageProps
  * Component: 각각의 페이지에 대한 컴포넌트, 페이지라고 보면 됨
  * pageProps: 컴포넌트에서 전달할 데이터를 담는 객체
- *  */ 
+ *  */
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Nav />
-    <Component {...pageProps} />;
+      <Header />
+      <Component {...pageProps} />;
     </>
   )
 }
