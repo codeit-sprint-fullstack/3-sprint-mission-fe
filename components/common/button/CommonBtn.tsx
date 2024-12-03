eac7ae35a282a77bbbc4dcb151edbc91b472f2b0
Daewony <alexnori.com@gmail.com>
2024-12-03 18:24:36 +0900

    style: 버튼 공통 컴포넌트 비활성화 텍스트 컬러 수정

--- components/common/button/CommonBtn.tsx
+++ components/common/button/CommonBtn.tsx
@@ -7,7 +7,7 @@ const CommonBtn = ({ disabled = false, children }: CommonBtnProps) => {
   return (
     <button
       className={`${
-        disabled ? "bg-gray text-gray" : "bg-blue text-blue-text"
+        disabled ? "bg-gray text-gray-light" : "bg-blue text-blue-text"
       } box-border h-[42px] text-nowrap rounded-lg px-6 text-base font-semibold`}
     >
       {children}
