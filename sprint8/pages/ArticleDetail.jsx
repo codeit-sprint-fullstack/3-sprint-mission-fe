52880adc2c7d40f30263e81e1d430f5df85c57c6
Jinhan <wlsgks333@naver.com>
2024-12-04 15:31:22 +0900

    feat: ArticleInfo 컴포넌트 디테일 페이지에 적용 완료

--- sprint8/pages/ArticleDetail.jsx
+++ sprint8/pages/ArticleDetail.jsx
@@ -1,9 +1,10 @@
 import styles from "@/styles/pages/ArticleDetail.module.css";
+import ArticleInfo from "@/components/ArticleDetail/ArticleInfo";
 
 function ArticleDetail() {
   return (
     <div className={styles.ArticleDetailBox}>
-      {/* ArticleInfo 정보가 들어갈 자리 */}
+      <ArticleInfo />
       {/* CommentPost댓글달기와 등록 버튼 자리 */}
       {/* Comments댓글 컴포넌트 */}
     </div>
