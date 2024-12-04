c7f8594bf11ff3c8e5fd1bb7789c18a16517ed26
Jinhan <wlsgks333@naver.com>
2024-12-04 15:54:30 +0900

    feat: CommentPost 컴포넌트 기본적인 디자인 css 구현 완료

--- sprint8/components/ArticleDetail/CommentPost.jsx
+++ sprint8/components/ArticleDetail/CommentPost.jsx
@@ -3,7 +3,21 @@ import styles from "@/styles/components/ArticleDetail/CommentPost.module.css";
 
 function CommentPost() {
   return (
-    <></>
+    <div className={styles.commentPostBox}>
+      <h2 className={styles.commentPostTitle}>댓글달기</h2>
+      <textarea
+        className={styles.textareaContent}
+        placeholder="댓글을 입력해주세요."
+        id="message"
+        name="message"
+        rows="4"
+        cols="50" />
+      <div className={styles.commentPostButtonBox}>
+        <button className={styles.commentPostButton}>
+          등록
+        </button>
+      </div>
+    </div>
   )
 }
 
