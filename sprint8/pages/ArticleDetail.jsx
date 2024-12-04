33686280150ace983645d6d6a01ffe6d00de5e68
Jinhan <wlsgks333@naver.com>
2024-12-04 10:49:19 +0900

    style: ArticleDetail 페이지 기본적 구조 구현

---
+++ sprint8/pages/ArticleDetail.jsx
@@ -0,0 +1,13 @@
+import styles from "@/styles/pages/ArticleDetail.module.css";
+
+function ArticleDetail() {
+  return (
+    <div className={styles.ArticleDetailBox}>
+      {/* ArticleInfo 정보가 들어갈 자리 */}
+      {/* CommentPost댓글달기와 등록 버튼 자리 */}
+      {/* Comments댓글 컴포넌트 */}
+    </div>
+  )
+}
+
+export default ArticleDetail;
