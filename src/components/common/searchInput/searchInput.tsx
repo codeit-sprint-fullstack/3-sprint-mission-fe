601f1ef183ef1dd757acf0f29dc4f1c99cc2652f
heonq <gkagjsrb@naver.com>
2024-12-04 16:43:43 +0900

    feat: 검색창 컴포넌트 수정

--- src/components/common/searchInput/searchInput.tsx
+++ src/components/common/searchInput/searchInput.tsx
@@ -1,15 +1,21 @@
 import cn from '@/lib/cn';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { InputHTMLAttributes } from 'react';
+import { SearchInputProps } from './searchInput.types';
+import { useState } from 'react';
 
 export default function SearchInput({
   className = '',
+  onSearch,
   ...props
-}: InputHTMLAttributes<HTMLInputElement>) {
+}: SearchInputProps) {
+  const [word, setWord] = useState('');
+
   return (
     <div className='w-full relative'>
       <input
+        value={word}
+        onChange={(e) => setWord(e.target.value)}
         className={cn(
           'bg-bg-input w-full px-[44px] py-[9px] rounded-[12px] text-text-charcoal-primary',
           className,
@@ -17,6 +23,7 @@ export default function SearchInput({
         {...props}
       />
       <FontAwesomeIcon
+        onClick={() => onSearch(word)}
         icon={faMagnifyingGlass}
         className='absolute left-5 top-[13px] text-bg-inputGlass cursor-pointer'
       />
