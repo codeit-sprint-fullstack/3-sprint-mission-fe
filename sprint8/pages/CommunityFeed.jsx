1306219c20adb765639e85268e8ed7d0b746e96f
Jinhan <wlsgks333@naver.com>
2024-12-04 12:30:13 +0900

    feat: '글쓰기' 버튼에 Link 태그 추가

--- sprint8/pages/CommunityFeed.jsx
+++ sprint8/pages/CommunityFeed.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "@/styles/pages/CommunityFeed.module.css";
 import BestArticles from "@/components/CommunityFeed/BestArticles";
 import ArticlesList from "@/components/CommunityFeed/ArticlesList"
@@ -6,14 +7,18 @@ function CommunityFeed() {
   return (
     <div className={styles.communityFeedBody}>
       <div className={styles.communityFeedMain}>
-      <h1 className={styles.communityFeedTitle}>베스트 게시글</h1>
+        <h1 className={styles.communityFeedTitle}>베스트 게시글</h1>
         <BestArticles />
-      <div className={styles.articlePostHeader}>
-        <h1 className={styles.communityFeedTitle}>게시글</h1>
-        <button className={styles.toArticlePostButton}>글쓰기</button>
-      </div>
-        <ArticlesList />
+        <div className={styles.articlePostHeader}>
+          <h1 className={styles.communityFeedTitle}>게시글</h1>
+          <Link href="/ArticlePost">
+            <button className={styles.toArticlePostButton}>
+              글쓰기
+            </button>
+          </Link>
         </div>
+        <ArticlesList />
+      </div>
     </div>
   )
 }
