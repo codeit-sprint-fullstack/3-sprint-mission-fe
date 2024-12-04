dca11fdc754677e7fa7253c9838954cff9af8cde
Jinhan <wlsgks333@naver.com>
2024-12-04 15:27:53 +0900

    style: 각각의 컴포넌트 스타일 파일 import

--- sprint8/components/ArticleDetail/CommentPost.jsx
+++ sprint8/components/ArticleDetail/CommentPost.jsx
@@ -1,3 +1,4 @@
+import styles from "@/styles/components/ArticleDetail/CommentPost.module.css";
 
 
 function CommentPost() {
--- sprint8/components/ArticleDetail/Comments.jsx
+++ sprint8/components/ArticleDetail/Comments.jsx
@@ -1,3 +1,4 @@
+import styles from "@/styles/components/ArticleDetail/Comments.module.css";
 
 
 function Comments() {
