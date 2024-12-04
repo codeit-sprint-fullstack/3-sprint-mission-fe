74bf1ea9f5a327e4330524b2f5af41630751f6fa
Jinhan <wlsgks333@naver.com>
2024-12-04 16:14:07 +0900

    feat: 목록으로 돌아가기 버튼 기본 디자인 구현 완료

--- sprint8/pages/ArticleDetail.jsx
+++ sprint8/pages/ArticleDetail.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Image from "next/image";
 import styles from "@/styles/pages/ArticleDetail.module.css";
 import ArticleInfo from "@/components/ArticleDetail/ArticleInfo";
 import CommentPost from "@/components/ArticleDetail/CommentPost";
@@ -9,6 +11,20 @@ function ArticleDetail() {
       <ArticleInfo />
       <CommentPost />
       <Comments />
+
+      <Link href="/CommunityFeed">
+        <button className={styles.toArticlePostButton}>
+          목록으로 돌아가기
+          <div className={styles.toArticlePostButtonImage}>
+            <Image
+              src="/images/icons/backArrow.png"
+              alt="목록으로 돌아가기 이미지"
+              width={19}
+              height={16}
+            />
+          </div>
+        </button>
+      </Link>
     </div>
   )
 }
