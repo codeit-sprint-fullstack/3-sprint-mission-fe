189583326ad649f4a7c18bdf9d507981fc372394
heonq <gkagjsrb@naver.com>
2024-12-05 22:33:35 +0900

    style: CommonBtn 컴포넌트 스타일 수정

--- src/components/common/commonBtn/commonBtn.tsx
+++ src/components/common/commonBtn/commonBtn.tsx
@@ -1,13 +1,18 @@
+import cn from '@/lib/cn';
 import { ButtonHTMLAttributes } from 'react';
 
 export default function CommonBtn({
   children,
+  className,
   ...props
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
-      className='w-[88px] h-[42px] bg-bg-button-active disabled:bg-bg-button-disabled text-white rounded-lg font-semibold'
+      className={cn(
+        'bg-bg-button-active disabled:bg-bg-button-disabled text-white rounded-lg font-semibold px-[23px] py-2',
+        className,
+      )}
     >
       {children}
     </button>
