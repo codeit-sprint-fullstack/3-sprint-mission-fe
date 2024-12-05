ffe15f255fd15105c846fd2009b00c2c19bec7fa
heonq <gkagjsrb@naver.com>
2024-12-05 15:26:19 +0900

    feat: 자유게시판 페이지 수정

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -3,10 +3,11 @@ import BestArticleListSection from '@/components/community/bestArticle/bestArtic
 import NormalArticleListSection from '@/components/community/normalArticle/normalArticleListSection';
 import SearchSection from '@/components/community/searchSection/searchSection';
 import { SearchParams } from '@/lib/types/searchParams';
+import Link from 'next/link';
 
 export default function Page({ searchParams }: { searchParams: SearchParams }) {
   return (
-    <div className='w-full md:w-full xl:w-[1248px] p-4 md:p-6 xl:p-6 flex flex-col'>
+    <div className='w-full md:w-full xl:w-[1200px] p-4 md:p-6 xl:p-0 xl:py-6 flex flex-col'>
       <span className='text-[18px] md:text-[24px] xl:text-[24px] font-extrabold mb-4 md:mb-6 xl:mb-6'>
         베스트 게시글
       </span>
@@ -17,7 +18,9 @@ export default function Page({ searchParams }: { searchParams: SearchParams }) {
         <span className='text-[18px] md:text-[24px] xl:text-[24px] font-extrabold mb-4 md:mb-6 xl:mb-6'>
           게시글
         </span>
-        <CommonBtn>글쓰기</CommonBtn>
+        <Link href='/community/write'>
+          <CommonBtn>글쓰기</CommonBtn>
+        </Link>
       </div>
       <SearchSection />
       <div className='flex flex-col gap-6'>
