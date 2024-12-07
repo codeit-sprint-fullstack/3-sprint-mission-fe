23d7eb790386e723e25edff4a70d979ad6b177c0
Jinhan <wlsgks333@naver.com>
2024-12-08 07:13:50 +0900

    feat: ArticlePost Page 구현

--- sprint8/pages/ArticlePost.jsx
+++ sprint8/pages/ArticlePost.jsx
@@ -1,6 +1,48 @@
+import { useState } from "react";
 import styles from "@/styles/pages/ArticlePost.module.css";
+import { postArticle } from "@/lib/pandaMarketApiService";
+import { set } from "date-fns";
 
 function ArticlePost() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [titleValid, setTitleValid] = useState(false);
+  const [contentValid, setContentValid] = useState(false);
+
+  const postHandler = async () => {
+    try {
+      if (title !== "" && content !== "") {
+        const response = await postArticle(title, content);
+        console.log("response : ", response);
+
+        if (response.ok) { // 요청이 성공한 후 입력 필드 초기화 
+          setInputValue('');
+          setTextareaValue('');
+        }
+      }
+      return;
+    } catch (error) {
+      console.error("Error posting article:", error);
+    }
+  }
+
+  const titleValidHandler = () => {
+    if (title === "") {
+      setTitleValid(true);
+    } else {
+      setTitleValid(false);
+    }
+  };
+
+  const contentValidHandler = () => {
+    if (content === "") {
+      setContentValid(true);
+    } else {
+      setContentValid(false);
+    }
+  };
+
+
   return (
     <div className={styles.ArticlePostContent}>
       <div className={styles.articlePostBox}>
@@ -8,26 +50,60 @@ function ArticlePost() {
           <h1 className={styles.articlePostHeaderTitle}>
             게시글 쓰기
           </h1>
-          <button className={styles.articlePostBoutton}>
+          <button
+            onClick={postHandler}
+            className={
+              `${styles.articlePostBoutton} ${title !== "" && content !== "" ? styles.postBouttonBlue : ""}`}
+          >
             등록
           </button>
         </div>
         <div className={styles.articlePostTitleBox}>
           <h2 className={styles.articlePostInputName}>*제목</h2>
 
-          <input className={styles.inputTitle} type="text" name="title" placeholder="제목을 입력해주세요"/>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            onBlur={titleValidHandler}
+            onClick={() => {
+              setContentValid(false)
+            }}
+            className={`${styles.inputTitle} ${titleValid ? styles.inputValidBox : ""}`}
+            type="text"
+            name="title"
+            placeholder="제목을 입력해주세요"
+          />
+          {titleValid ?
+            <div className={styles.inputValidText}>
+              제목을 입력해주세요
+            </div>
+            : null
+          }
 
         </div>
         <div className={styles.articlePostContentBox}>
           <h2 className={styles.articlePostInputName}>*내용</h2>
 
           <textarea
-            className={styles.textareaContent}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            onBlur={contentValidHandler}
+            onClick={() => {
+              setTitleValid(false)
+            }}
+            className={`${styles.textareaContent} ${contentValid ? styles.inputValidBox : ""}`}
             placeholder="내용을 입력해주세요"
             id="message"
             name="message"
             rows="4"
             cols="50" />
+
+          {contentValid ?
+            <div className={styles.inputValidText}>
+              내용을 입력해주세요
+            </div>
+            : null
+          }
 
         </div>
       </div>
