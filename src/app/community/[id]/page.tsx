366eff7feba8c13dfad43bb733e7d313c97a63db
heonq <gkagjsrb@naver.com>
2024-12-07 12:42:24 +0900

    feat: 게시글 상세페이지 수정

--- src/app/community/[id]/page.tsx
+++ src/app/community/[id]/page.tsx
@@ -1,27 +1,20 @@
-import ArticleHeader from '@/components/community/articlePage/articleHeader';
-import CommentForm from '@/components/community/articlePage/commentForm';
 import { getArticle, getArticleComments } from '@/services/api/article';
-import CommentContainer from '@/components/community/comment/commentContainer';
-import NoComment from '@/components/community/comment/noComment';
 import { ParamsProps } from '@/lib/types/paramsProps.types';
 import ReturnButton from '@/components/community/articlePage/returnButton';
+import ArticleClient from '@/components/community/articlePage/articleClient';
+import CommentListClient from '@/components/community/comment/commentListClient';
 
 export default async function Page({ params }: ParamsProps) {
   const article = await getArticle(params.id);
-  const { data } = await getArticleComments(params.id);
+  const comments = await getArticleComments(params.id);
 
   return (
     <article className='w-full md:w-full xl:w-[1200px] p-4 md:p-6 xl:p-0 xl:py-6 flex flex-col'>
-      <ArticleHeader
-        id={article.id}
-        nickname='판다'
-        title={article.title}
-        createdAt={article.createdAt}
+      <ArticleClient initialData={article} />
+      <CommentListClient
+        articleId={article.id}
+        initialData={comments}
       />
-      <div className='mb-8'>{article.content}</div>
-      <CommentForm articleId={params.id} />
-      {data.length > 0 ? <CommentContainer data={data} /> : <NoComment />}
-
       <ReturnButton />
     </article>
   );
