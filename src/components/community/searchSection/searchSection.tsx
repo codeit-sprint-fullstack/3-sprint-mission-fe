ab1c86b623937fe82c45ecfd8080cbdaa99f088c
heonq <gkagjsrb@naver.com>
2024-12-08 14:24:57 +0900

    feat: SearchSection 컴포넌트에 정렬 기능 추가

--- src/components/community/searchSection/searchSection.tsx
+++ src/components/community/searchSection/searchSection.tsx
@@ -3,18 +3,22 @@
 import { useRouter } from 'next/navigation';
 import CustomSelect from '../../common/customSelect/customSelect';
 import SearchInput from '../../common/searchInput/searchInput';
+import { useState } from 'react';
+
+const options = [
+  { label: '최신순', value: 'recent' },
+  { label: '좋아요순', value: 'likes' },
+];
 
 export default function SearchSection() {
-  const options = [
-    { label: '최신순', value: 'recent' },
-    { label: '좋아요순', value: 'likes' },
-  ];
+  const [orderBy, setOrderBy] = useState(options[0].value);
 
   const router = useRouter();
 
   const onSearch = (word: string) => {
-    if (word) return router.push(`?word=${encodeURIComponent(word)}`);
-    return router.push('?');
+    let queryParams = `?orderBy=${encodeURIComponent(orderBy)}`;
+    if (word) queryParams += `&word=${encodeURIComponent(word)}`;
+    router.push(queryParams);
   };
 
   return (
@@ -22,7 +26,7 @@ export default function SearchSection() {
       <div className='flex gap-4 mb-6'>
         <SearchInput
           onSearch={onSearch}
-          placeholder='검색할 상품을 입력해주세요'
+          placeholder='검색할 게시글을 입력해주세요'
         />
         <CustomSelect
           options={options}
