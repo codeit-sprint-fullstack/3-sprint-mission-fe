f2c3472765f87add9ccbeb2377c1349c8a82a845
Jinhan <wlsgks333@naver.com>
2024-12-08 12:00:27 +0900

    feat: CommentPost 기능 구현

--- sprint8/components/ArticleDetail/CommentPost.jsx
+++ sprint8/components/ArticleDetail/CommentPost.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import styles from "@/styles/components/ArticleDetail/CommentPost.module.css";
+import { postComment } from "@/lib/pandaMarketApiService";
 
+function CommentPost({ articleId, commentPost }) {
+  const [textareaValue, setTextareaValue] = useState("");
 
-function CommentPost() {
+  const handlePostComment = async (e) => {
+    e.preventDefault();
+    const response = await postComment(articleId, textareaValue);
+    console.log("response : ", response);
+    commentPost(response)
+    setTextareaValue('');
+  }
+
   return (
     <div className={styles.commentPostBox}>
       <h2 className={styles.commentPostTitle}>댓글달기</h2>
       <textarea
+        value={textareaValue}
+        onChange={(e) => setTextareaValue(e.target.value)}
         className={styles.textareaContent}
         placeholder="댓글을 입력해주세요."
         id="message"
@@ -13,7 +26,11 @@ function CommentPost() {
         rows="4"
         cols="50" />
       <div className={styles.commentPostButtonBox}>
-        <button className={styles.commentPostButton}>
+        <button
+          onClick={handlePostComment}
+          className={`
+          ${styles.commentPostButton}
+          ${textareaValue ? styles.postButtonBlue : ''}`}>
           등록
         </button>
       </div>
