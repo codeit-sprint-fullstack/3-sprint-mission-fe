6bfd937f8b3abcc85842ea9d0f5b12758a9bdba4
Jinhan <wlsgks333@naver.com>
2024-12-08 12:08:41 +0900

    feat: getArticleId, getComments, postComment 함수 구현

--- sprint8/lib/pandaMarketApiService.js
+++ sprint8/lib/pandaMarketApiService.js
@@ -43,6 +43,39 @@ const postArticle = async (title, content) => {
   }
 };
 
+const getArticleId = async (articleId) => {
+  try {
+    const { data } = await axios.get(`/articles/${articleId}`);
+    return data;
+  } catch (error) {
+    console.error('Error fetching article by ID:', error);
+    throw error;
+  }
+};
+
+const getComments = async (articleId) => {
+  try {
+    const { data } = await axios.get(`/articles/${articleId}/comments`);
+    return data;
+  } catch (error) {
+    console.error('Error fetching comments:', error);
+    throw error;
+  }
+};
+
+// 게시글 댓글 등록
+// app.post('/articles/:articleId/comments',
+const postComment = async (articleId, content) => {
+  try {
+    const { data } = await axios.post(`/articles/${articleId}/comments`, {
+      content,
+    });
+    return data;
+  } catch (error) {
+    console.error('Error posting comment:', error.response?.data || error.message);
+    throw error;
+  }
+};
 
 
-export { getProducts, getProductId, getArticles, postArticle };
+export { getProducts, getProductId, getArticles, postArticle, getArticleId, getComments, postComment };
