9eebe828e8cdd473f8d0a140def919ad7ff9f6ca
Jinhan <wlsgks333@naver.com>
2024-12-08 12:02:03 +0900

    feat: 댓글 데이터 정보 렌더링

--- sprint8/components/ArticleDetail/Comments.jsx
+++ sprint8/components/ArticleDetail/Comments.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "@/styles/components/ArticleDetail/Comments.module.css";
+import formatDate from "@/lib/formatDate";
 
 
-function Comments() {
+function Comments({comment}) {
   return (
     <div className={styles.commentsBox}>
       <div className={styles.commentsHeader}>
-        <h2 className={styles.comments}>혹시 사용기간이 어떻게 되실까요?
+        <h2 className={styles.comments}>
+          {comment.content}
         </h2>
         <div className={styles.togleMenuMark}>⋮</div>
       </div>
@@ -25,7 +27,7 @@ function Comments() {
         </div>
         <div className={styles.metaDataSmallBox}>
           <div className={styles.nickname}>똑똑한판다</div>
-          <div className={styles.date}>1시간 전</div>
+          <div className={styles.date}>{formatDate(comment.createdAt)}</div>
         </div>
       </div>
 
