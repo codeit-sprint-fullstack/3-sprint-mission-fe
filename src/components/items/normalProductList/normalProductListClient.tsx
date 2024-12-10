26726a852a5556353279297ddb8ce662434f46ec
heonq <gkagjsrb@naver.com>
2024-12-10 17:29:33 +0900

    feat: pageIndex 컴포넌트 구현

--- src/components/items/normalProductList/normalProductListClient.tsx
+++ src/components/items/normalProductList/normalProductListClient.tsx
@@ -7,21 +7,25 @@ import Product from '../product/product';
 import { useAtom } from 'jotai';
 import { screenWidthAtom } from '@/lib/store/atoms';
 import { MEDIA_QUERY } from '@/constants/mediaQuery';
+import cn from '@/lib/cn';
+import PageIndex from '@/components/common/pageIndex/pageIndex';
 
-const TAKE_VALUE = {
-  [MEDIA_QUERY.value.large]: 10,
-  [MEDIA_QUERY.value.medium]: 6,
-  [MEDIA_QUERY.value.small]: 4,
+const GRID_COLS = {
+  [MEDIA_QUERY.value.large]: 'grid-cols-5',
+  [MEDIA_QUERY.value.medium]: 'grid-cols-3',
+  [MEDIA_QUERY.value.small]: 'grid-cols-2',
 };
 
 export default function NormalProductListClient({
   searchParams,
 }: NormalProductListClientProps) {
   const [screenWidth] = useAtom(screenWidthAtom);
-  const take = Number(TAKE_VALUE[screenWidth || MEDIA_QUERY.value.large]);
+  const take = Number(
+    MEDIA_QUERY.productsPageSize[screenWidth || MEDIA_QUERY.value.large],
+  );
 
   const { data } = useQuery({
-    queryKey: ['products', searchParams.word, take],
+    queryKey: ['products', searchParams.word, searchParams.skip, take],
     queryFn: () =>
       getProductList({
         skip: Number(searchParams.skip) || 0,
@@ -32,18 +36,27 @@ export default function NormalProductListClient({
     enabled: !!screenWidth,
   });
 
-  return (
-    <div className='w-full flex flex-wrap gap-1 md:gap-4 xl:gap-5 mb-[223px]'>
-      {data?.data.map((product) => (
-        <Product
-          key={product.id}
-          image='mockImage'
-          title={product.name}
-          price={product.price}
-          likes={99}
-          size='small'
-        />
-      ))}
-    </div>
-  );
+  if (data)
+    return (
+      <div className='flex flex-col items-center mb-[165px]'>
+        <div
+          className={cn(
+            'grid gap-1 items-center md:gap-4 xl:gap-5 mb-10',
+            GRID_COLS[screenWidth || MEDIA_QUERY.value.large],
+          )}
+        >
+          {data.data.map((product) => (
+            <Product
+              key={product.id}
+              image='mockImage'
+              title={product.name}
+              price={product.price}
+              likes={99}
+              size='small'
+            />
+          ))}
+        </div>
+        <PageIndex maxPage={data.count / (searchParams.take ?? 10) + 1} />
+      </div>
+    );
 }
