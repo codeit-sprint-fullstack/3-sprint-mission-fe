e6f450999ea32b03b5e71603088610d0dc19f651
heonq <gkagjsrb@naver.com>
2024-12-16 16:35:26 +0900

    feat: 소셜로그인 이미지 클릭 시 경로 이동 기능 구현

--- src/components/sign/socialLogin.tsx
+++ src/components/sign/socialLogin.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import kakao from '@/public/images/sign/kakaotalk.png';
 import google from '@/public/images/sign/google.png';
+import Link from 'next/link';
 
 export default function SocialLogin() {
   return (
@@ -8,20 +9,24 @@ export default function SocialLogin() {
       <div className='flex justify-between w-full bg-bg-social-login items-center px-[23px] py-4 font-medium rounded-lg'>
         <span>간편 로그인하기</span>
         <div className='flex items-center gap-4'>
-          <Image
-            src={google}
-            alt='구글아이디로 로그인'
-            width={42}
-            height={42}
-            className='cursor-pointer'
-          />
-          <Image
-            src={kakao}
-            alt='카카오 계정으로 로그인'
-            width={42}
-            height={42}
-            className='cursor-pointer'
-          />
+          <Link href='https://www.google.com'>
+            <Image
+              src={google}
+              alt='구글아이디로 로그인'
+              width={42}
+              height={42}
+              className='cursor-pointer'
+            />
+          </Link>
+          <Link href='https://www.kakaocorp.com/page'>
+            <Image
+              src={kakao}
+              alt='카카오 계정으로 로그인'
+              width={42}
+              height={42}
+              className='cursor-pointer'
+            />
+          </Link>
         </div>
       </div>
     </>
