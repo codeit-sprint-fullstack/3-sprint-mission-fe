f45cef750649b8b77ed5d8231776d5470ac52c42
heonq <gkagjsrb@naver.com>
2024-12-19 17:49:56 +0900

    refactor: 상품 페이지에 불필요한 코드 제거

--- src/app/(routes)/items/[id]/page.tsx
+++ src/app/(routes)/items/[id]/page.tsx
@@ -2,11 +2,8 @@ import CommentContainer from '@/components/articlePage/comment/commentContainer'
 import ReturnButton from '@/components/articlePage/returnButton/returnButton';
 import ProductInformationSection from '@/components/items/itemPage/productInformationSection';
 import { ParamsProps } from '@/lib/types/paramsProps.types';
-import { getProduct } from '@/services/api/product';
 
-export default async function ProductPage({ params }: ParamsProps) {
-  const product = await getProduct(params.id);
-
+export default function ProductPage({ params }: ParamsProps) {
   return (
     <div className='w-full md:w-full xl:w-[1200px] p-4 md:p-6 xl:p-0 xl:py-6 flex flex-col'>
       <ProductInformationSection id={params.id} />
