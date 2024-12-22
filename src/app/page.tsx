492f3a9895d39d06512c84d60fc256b13ed06ef3
KJY <139238536+dani784601@users.noreply.github.com>
2024-12-22 11:41:06 +0900

    feat: 초기 설정 및 Tailwind CSS, ESLint, TypeScript 구성 추가

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <main >
+
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
