753449a5f7c271af0f2b35ecdb10d520c740bb08
heonq <gkagjsrb@naver.com>
2024-12-22 15:47:48 +0900

    feat: 회원가입 시 가입 완료 메시지 표시 기능 추가

--- src/hooks/useAuthMutation.ts
+++ src/hooks/useAuthMutation.ts
@@ -4,7 +4,7 @@ import { AxiosError } from 'axios';
 import { useRouter } from 'next/navigation';
 import { useMessageModal } from './modals/useMessageModal';
 
-export const useAuthMutation = <T>(
+export const useAuthMutation = <T extends object>(
   authFn: (data: T) => Promise<SignInResponse>,
 ) => {
   const router = useRouter();
@@ -13,11 +13,12 @@ export const useAuthMutation = <T>(
 
   return useMutation<SignInResponse, AxiosError<{ message: string }>, T>({
     mutationFn: authFn,
-    onSuccess: (response) => {
+    onSuccess: (response, variable) => {
       localStorage.setItem('accessToken', response.accessToken);
       localStorage.setItem('refreshToken', response.refreshToken);
       queryClient.setQueriesData({ queryKey: ['me'] }, response.user);
-      router.push('/items');
+      if ('passwordConfirmation' in variable)
+        setMessage('가입 완료되었습니다', () => router.push('/items'));
     },
     onError: (error) => {
       setMessage(error?.response?.data.message || '오류가 발생했습니다.');
