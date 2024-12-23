42331c8870f939b974a4e2fb9f218db4a324f1f0
heonq <gkagjsrb@naver.com>
2024-12-23 16:11:35 +0900

    fix: useRouter 잘못된 import 경로 수정

--- src/hooks/auth/useAuthRedirect.ts
+++ src/hooks/auth/useAuthRedirect.ts
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useMe } from '../useMe';
 import { useMessageModal } from '../modals/useMessageModal';
 
