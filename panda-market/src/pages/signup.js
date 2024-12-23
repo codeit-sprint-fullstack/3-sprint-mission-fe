93d1e3e07b3622278b0427633303505b7d173906
Aventurine91 <mandely01@gmail.com>
2024-12-23 10:33:57 +0900

    회원가입페이지완료

--- panda-market/src/pages/signup.js
+++ panda-market/src/pages/signup.js
@@ -1,19 +1,80 @@
+import { useState } from "react";
 import styles from "../styles/Signup.module.css";
 
 export default function Signup() {
+    const [showPassword, setShowPassword] = useState(false);
+  
+    // 비밀번호 보이기/숨기기 토글 함수
+    const togglePasswordVisibility = () => {
+      setShowPassword((prev) => !prev);
+    };
+  
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <img src="/logo.png" alt="판다마켓 로고" className={styles.logo} />
+        <div className={styles.logo}>
+        <img src="/panda_logo.png" alt="판다마켓 로고"  />
         <h1>판다마켓</h1>
-        <form>
-          <input type="email" placeholder="이메일을 입력해주세요" />
-          <input type="text" placeholder="닉네임을 입력해주세요" />
-          <input type="password" placeholder="비밀번호를 입력해주세요" />
-          <input type="password" placeholder="비밀번호를 다시 입력해주세요" />
-          <button type="submit">회원가입</button>
+        </div>
+        <form className={styles.form}>
+
+          <p>이메일</p>
+          <input
+            type="email"
+            className={styles.input}
+            placeholder="이메일을 입력해주세요"
+          />
+
+          <p>닉네임</p>
+          <input
+            type="nickname"
+            className={styles.input}
+            placeholder="이메일을 입력해주세요"
+          />
+      
+          <p>비밀번호</p>
+          <div className={styles.passwordContainer}>
+            <input
+              type={showPassword ? "text" : "password"}
+              className={styles.input}
+              placeholder="비밀번호를 입력해주세요"
+            />
+            <img
+              src={showPassword ? "/ic_eye_closed.png" : "/ic_eye_closed.png"}
+              alt="비밀번호 보이기/숨기기"
+              className={styles.eyeIcon}
+              onClick={togglePasswordVisibility}
+            />
+          </div>
+
+          <p>비밀번호 확인</p>
+          <div className={styles.passwordContainer}>
+            <input
+              type={showPassword ? "text" : "password"}
+              className={styles.input}
+              placeholder="비밀번호를 입력해주세요"
+            />
+            <img
+              src={showPassword ? "/ic_eye_closed.png" : "/ic_eye_closed.png"}
+              alt="비밀번호 보이기/숨기기"
+              className={styles.eyeIcon}
+              onClick={togglePasswordVisibility}
+            />
+          </div>
+          <button className={styles.button} type="submit">
+            회원가입
+          </button>
         </form>
-        <p>
+
+        <div className={styles.socialLogin}>
+          <p>간편 로그인하기</p>
+          <div className={styles.icons}>
+            <img src="/ic_google.png" alt="Google 로그인" />
+            <img src="/ic_kakao.png" alt="Kakao 로그인" />
+          </div>
+        </div>
+
+        <p className={styles.text}>
           이미 회원이신가요? <a href="/login">로그인</a>
         </p>
       </div>
