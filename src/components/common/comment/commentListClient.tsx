1bc77ac80f726fc7920dd127057fb1ebe13c0d2d
heonq <gkagjsrb@naver.com>
2024-12-29 22:28:42 +0900

    feat: api 변경에 따른 변경사항 적용

--- src/components/common/comment/commentListClient.tsx
+++ src/components/common/comment/commentListClient.tsx
@@ -3,25 +3,40 @@
 import Comment from './comment';
 import profileIcon from '@/public/icons/profile_icon.png';
 import { useCommentsQuery } from '@/hooks/comments/useCommentsQuery';
-import { WithInitialData } from '@/lib/types/params.types';
+import { CommentListParams } from '@/lib/types/params.types';
+import CommentForm from '../commentForm/commentForm';
+import CommentSkeletonList from './commentSkeletonList';
+import NoComment from './noComment';
 
-export default function CommentListClient(props: WithInitialData) {
-  const { data: comments } = useCommentsQuery(props);
+export default function CommentListClient(params: CommentListParams) {
+  const { data: comments, isLoading } = useCommentsQuery(params);
 
   return (
-    <div className='mb-10'>
-      {comments.list.map((comment) => (
-        <Comment
-          variant={props.variant}
-          key={comment.id}
-          id={comment.id}
-          pageId={props.id}
-          nickname={comment.writer.nickname}
-          createdAt={comment.createdAt}
-          content={comment.content}
-          profileIcon={comment.writer.image || profileIcon}
-        />
-      ))}
-    </div>
+    <>
+      <CommentForm
+        id={params.id}
+        variant={params.variant}
+      />
+      <div className='mb-10'>
+        {isLoading ? (
+          <CommentSkeletonList />
+        ) : comments && comments.list.length > 0 ? (
+          comments.list.map((comment) => (
+            <Comment
+              variant={params.variant}
+              key={comment.id}
+              id={comment.id}
+              pageId={params.id}
+              nickname={comment.user.nickname}
+              createdAt={comment.createdAt}
+              content={comment.content}
+              profileIcon={comment.user.image || profileIcon}
+            />
+          ))
+        ) : (
+          <NoComment variant={params.variant} />
+        )}
+      </div>
+    </>
   );
 }
