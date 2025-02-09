a5684ff457ba97da70ff8d8aafb8e8a7da5ab535
Daewon Kang <alexnori.com@gmail.com>
2025-02-09 17:21:43 +0900

    refactor(HomeBanner):  dangerouslySetInnerHTML 불필요한 속성 제거

--- components/home/HomeBanner.tsx
+++ components/home/HomeBanner.tsx
@@ -21,10 +21,9 @@ const HomeBanner = ({
     <section className="box-border h-[540px] w-full bg-[#cfe5ff] sm:h-[540px]">
       <figure className="mx-auto flex h-full items-end justify-center">
         <div className="mb-[90px] max-w-[400px]">
-          <h1
-            className="mb-[4rem] max-w-[35rem] whitespace-normal break-keep text-center text-[4rem] font-bold leading-[5.6rem] sm:max-w-[24rem] sm:text-center sm:text-[3.2rem] sm:leading-[4rem] md:max-w-full"
-            dangerouslySetInnerHTML={{ __html: title }}
-          />
+          <h1 className="mb-[4rem] max-w-[35rem] whitespace-normal break-keep text-center text-[4rem] font-bold leading-[5.6rem] sm:max-w-[24rem] sm:text-center sm:text-[3.2rem] sm:leading-[4rem] md:max-w-full">
+            {title}
+          </h1>
           {!isFooter && (
             <Link href={buttonLink}>
               <button className="h-[5.6rem] w-full max-w-[36rem] cursor-pointer rounded-[4rem] border-none bg-blue px-[11rem] py-[1.6rem] text-[2rem] font-semibold text-white sm:px-[7rem] sm:py-[1rem] sm:text-[1.8rem]">
